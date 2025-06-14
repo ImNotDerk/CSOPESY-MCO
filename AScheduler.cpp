7f177cf27be7a67f03f54b3ace9a23f357e41e94
Hans Jared Sescon <hjcsescon@gmail.com>
2025-06-14 20:03:45 +0800

    Add AScheduler class for process scheduling
    Introduced the `AScheduler` class in `AScheduler.cpp` and `AScheduler.h`, implementing a First-Come, First-Served (FCFS) scheduling algorithm. The class inherits from `IETThread` and includes methods for managing processes, including finding, running, and stopping them. Updated `MCO.vcxproj` to include the new source and header files for compilation.

--- AScheduler.cpp
+++ AScheduler.cpp
@@ -1,1 +1,33 @@
 #include "AScheduler.h"
+
+AScheduler::AScheduler(SchedulingAlgorithm schedulingAlgo, int pid, String processName)
+{
+	this->schedulingAlgo = schedulingAlgo;
+	this->pid = pid;
+	this->processName = processName;
+}
+
+std::shared_ptr<Process> AScheduler::findProcess()
+{
+	for (auto process : this->processes)
+	{
+		if (process->getName() == this->processName)
+		{
+			return process;
+		}
+	}
+}
+
+void AScheduler::run()
+{
+	this->init();
+	while (true) 
+	{
+		this->execute();
+	}
+}
+
+void AScheduler::stop()
+{
+	this->running = false;
+}
--- AScheduler.h
+++ AScheduler.h
@@ -1,18 +1,54 @@
 #pragma once
 #include "IETThread.h"
+#include "Process.h"
+
 #include <sstream>
-#include <unordered_map>
-#include <string>
+#include <iostream>
 
 typedef std::string String;
 
-class AScheduler
+static const String FCFS_SCHEDULER_NAME = "FCFSScheduler";
+
+class AScheduler : public IETThread
 {
 public:
+	enum SchedulingAlgorithm
+	{
+		FCFS
+	};
 
+	AScheduler();
+	AScheduler(SchedulingAlgorithm schedulingAglo, int pid, String processName);
+	~AScheduler() = default;
 
+	std::shared_ptr<Process> findProcess(String processName);
+	void run() override;
+	void stop();
+
+	virtual int checkCores() = 0;
+	virtual void init() = 0;
+	virtual void execute() = 0;
+	virtual void addProcess(std::shared_ptr<Process> process, int core) = 0;
+	virtual void assignCore(std::shared_ptr<Process> process, int core) = 0;
+	virtual int checkCoreQueue() = 0;
+	virtual String getProcessfromQueue(int index) const = 0;
+	virtual void printCore() = 0;
+	virtual void printProcessQueue() = 0;
+
+	virtual void schedulerStart() = 0;
+	virtual void schedulerStop() = 0;
+
+	virtual String getName() const = 0;
+
+	friend class GlobalScheduler;
+	
 private:
+	SchedulingAlgorithm schedulingAlgo;
+	int pid;
+	String processName;
+	bool running = true;
 
-
+protected:
+	std::vector<std::shared_ptr<Process>> processes;
 };
 
