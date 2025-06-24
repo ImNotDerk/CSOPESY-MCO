b9d7aaaa8f9b5873067670c3288408dec15c33d5
Hans Jared Sescon <hjcsescon@gmail.com>
2025-06-25 01:36:37 +0800

    Implement Add and Subtract command functionalities
    Updated AddCommand and SubtractCommand classes to include
    execute methods that perform addition and subtraction,
    respectively, and print the results. Added performAddition
    and performSubtraction methods for the respective logic.
    Modified ICommand class to include the ADD command type and defined its methods in ICommand.cpp.

--- AddCommand.h
+++ AddCommand.h
@@ -1,5 +1,21 @@
 #pragma once
-class AddCommand
+#include "ICommand.h"
+class AddCommand : public ICommand
 {
+public:
+	AddCommand(int processID, uint16_t var1, uint16_t var2, uint16_t var3) : ICommand(processID, CommandType::ADD)
+	{
+		this->var1 = var1;
+		this->var2 = var2;
+		this->var3 = var3;
+	}
+
+	uint16_t performAddition();
+	void execute() override;
+
+private:
+	uint16_t var1; // result
+	uint16_t var2; // value 1
+	uint16_t var3; // value 2
 };
 
--- SubtractCommand.h
+++ SubtractCommand.h
@@ -1,5 +1,21 @@
 #pragma once
-class SubtractCommand
+#include "ICommand.h"
+class SubtractCommand : public ICommand
 {
+public:
+	SubtractCommand(int processID, uint16_t var1, uint16_t var2, uint16_t var3) : ICommand(processID, CommandType::ADD)
+	{
+		this->var1 = var1;
+		this->var2 = var2;
+		this->var3 = var3;
+	}
+
+	uint16_t performSubtraction();
+	void execute() override;
+
+private:
+	uint16_t var1; // result
+	uint16_t var2; // value 1
+	uint16_t var3; // value 2
 };
 
