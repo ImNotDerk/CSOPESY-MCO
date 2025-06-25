56e1a24cd5a1c5cd88982c603eace285cbe29617
yuunisan <ryu_unisan@dlsu.edu.ph>
2025-06-26 01:27:18 +0800

    SchedulerWorker functionality
    Updated project files to include GlobalScheduler.cpp and GlobalScheduler.h.
    Modified filters for ICommand.cpp and added new files for proper organization.
    Enhanced SchedulerWorker with new methods for process management and restructured its header file to improve design.

--- SchedulerWorker.cpp
+++ SchedulerWorker.cpp
@@ -1,14 +1,41 @@
 #include "SchedulerWorker.h"
 //#include "GlobalScheduler.h"
 
-void SchedulerWorker::update(bool isRunning)
-{
+void SchedulerWorker::update(bool isRunning) {
 	this->isRunning = isRunning;
 }
 
-void SchedulerWorker::run()
-{
+void SchedulerWorker::run() {
 	while (isRunning) {
 		//GlobalScheduler::getInstance()->tick();
+		//for memory allocation
 	}
 }
+
+void SchedulerWorker::addProcess(std::shared_ptr<Process> process) {
+	this->processQueue.push(process);
+	this->currentProcess = processQueue.front();
+}
+
+void SchedulerWorker::isOccupied() {
+
+	this->available = false;
+}
+
+bool SchedulerWorker::isAvailable() const {
+	return this->available;
+}
+
+bool SchedulerWorker::doesProcessExist() const {
+	if (this->currentProcess != nullptr) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+int SchedulerWorker::getNumCores() const {
+	return this->numCores; 
+}
+
--- SchedulerWorker.h
+++ SchedulerWorker.h
@@ -1,13 +1,26 @@
-#pragma once
-#include "IETThread.h"
-class SchedulerWorker : public IETThread
-{
-public:
-	SchedulerWorker() = default;
-	void update(bool isRunning);
-	void run() override;
+#pragma once  
+#include "IETThread.h"  
+#include "Process.h"  
+#include <thread>  
+#include <mutex>  
+#include <queue>  
 
-private:
-	bool isRunning = true;
+class SchedulerWorker : public IETThread  
+{  
+public:  
+    SchedulerWorker() = default;  
+    void update(bool isRunning);  
+    void run() override;  
+    void isOccupied();  
+    void addProcess(std::shared_ptr<Process> process);  
+    bool isAvailable() const;  
+    bool doesProcessExist() const;  
+    int getNumCores() const;  
+
+private:  
+    bool isRunning = true;  
+    bool available = true;  
+    int numCores;  
+    std::shared_ptr<Process> currentProcess; 
+    std::queue<std::shared_ptr<Process>> processQueue; 
 };
-
